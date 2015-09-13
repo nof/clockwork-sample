0a9b1efe3648550a964a20208b151230676a3e1b
nof <sakape@gmail.com>
2015-09-13 20:56:52 +0900

    hourに

--- lib/clock.rb
+++ lib/clock.rb
@@ -5,5 +5,6 @@ require 'clockwork'
 include Clockwork
 
 # every(30.seconds, 'post current time per 30sec') { Post.post_current_time 'per 30sec' }
-every(30.minutes, 'post current time per 5min') { Post.post_current_time 'per 30min' }
+# every(30.minutes, 'post current time per 30min') { Post.post_current_time 'per 30min' }
+every(1.hour, 'post current time per hour', :at => '**:00') { Post.post_current_time 'per hour' }
 every(1.day, 'post current time per day', at: '21:00') { Post.post_current_time 'per day' }
